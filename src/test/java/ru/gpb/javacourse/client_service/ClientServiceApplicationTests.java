b6f8e72a5d38e57da4494dd51f6397f748dca380
Dzhakhbarov_Vladimir <dzahbarovvladimir@gmail.com>
2022-01-28 22:02:50 +0300

    init commit

---
+++ src/test/java/ru/gpb/javacourse/client_service/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gpb.javacourse.client_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
